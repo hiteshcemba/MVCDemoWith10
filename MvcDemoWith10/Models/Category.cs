d1e8c449ff80ce9bfbfaac8cb5b701e0ad45ddf0
hiteshcemba <hitesh.cemba@outlook.com>
2015-11-17 11:34:21 -0500

    commit 1

---
+++ MvcDemoWith10/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcDemoWith10.Models
+{
+
+    public class Category
+    {
+
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+        public string Email { get; set; }
+    }
+    public class CategoryDBContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+    }
+
+
+}
